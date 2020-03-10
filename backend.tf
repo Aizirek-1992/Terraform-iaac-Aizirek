4ee01428a94800894d419c3584b567dbe108f713
Cloud User <centos@ip-172-31-16-140.eu-west-2.compute.internal>
2020-03-10 21:25:13 +0000

    added

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-class-aizirek"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-     dynamodb_table = "terraform-class"
-     }
--- s31.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-Aizirek"
-}
-
-resource "aws_s3_bucket" "basdf" {
-  bucket = "my-tf-test-bucket-Aizirek"
-}
