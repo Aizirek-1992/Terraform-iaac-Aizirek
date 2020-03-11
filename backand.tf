1f50a0c3907e91836f1b5c98722113830a927663
Aizirek T <aizirekt98@gmail.com>
2020-03-11 18:34:51 -0500

    1

--- backand.tf
+++ backand.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-class-andrii"
+    bucket = "terraform-class-aizirek"
     key    = "path/to/my/key"
     region = "us-east-1"
     #dynamodb_table = "terraform-class"
